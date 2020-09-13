71a5e81ceab499d7921acd5763a10f90406adc6b
Bernard Maltais <bernard.maltais@canada.ca>
2020-09-13 21:17:50 +0000

    Fix issue

--- scripts/build_image_ssc.sh
+++ scripts/build_image_ssc.sh
@@ -27,27 +27,27 @@ sudo docker-compose build --build-arg versionRover=${rover}
 
 case "$1" in 
     "github")
-        sudo docker tag rover_rover ${rover}
-        sudo docker tag rover_rover sscspccloudnuage/rover:latest
+        docker tag rover_rover ${rover}
+        docker tag rover_rover sscspccloudnuage/rover:latest
 
-        sudo docker push sscspccloudnuage/rover:${tag}
-        sudo docker push sscspccloudnuage/rover:latest
+        docker push sscspccloudnuage/rover:${tag}
+        docker push sscspccloudnuage/rover:latest
 
         echo "Version sscspccloudnuage/rover:${tag} created."
         echo "Version sscspccloudnuage/rover:latest created."
         ;;
     "dev")
-        sudo docker tag rover_rover sscspccloudnuage/roverdev:${tag}
-        sudo docker tag rover_rover sscspccloudnuage/roverdev:latest
+        docker tag rover_rover sscspccloudnuage/roverdev:${tag}
+        docker tag rover_rover sscspccloudnuage/roverdev:latest
 
-        sudo docker push sscspccloudnuage/roverdev:${tag}
-        sudo docker push sscspccloudnuage/roverdev:latest
+        docker push sscspccloudnuage/roverdev:${tag}
+        docker push sscspccloudnuage/roverdev:latest
         echo "Version sscspccloudnuage/roverdev:${tag} created."
         echo "Version sscspccloudnuage/roverdev:latest created."
         ;;
     *)    
-        sudo docker tag rover_rover sscspccloudnuage/rover:$tag
-        sudo docker tag rover_rover sscspccloudnuage/rover:latest
+        docker tag rover_rover sscspccloudnuage/rover:$tag
+        docker tag rover_rover sscspccloudnuage/rover:latest
         echo "Local version created"
         echo "Version sscspccloudnuage/rover:${tag} created."
         echo "Version sscspccloudnuage/roverdev:latest created."
