2b8231648666627a6c3e2931c112a94078ffd1f2
Bernard Maltais <bernard.maltais@canada.ca>
2021-02-03 08:06:30 -0500

    Update runactions

--- scripts-rover/runactions.sh
+++ scripts-rover/runactions.sh
@@ -3,20 +3,21 @@
 # command can be plan, apply, destroy or validate
 env=${1}
 
-command=${2}
+# command=${2}
 blueprint=${PWD##*/}
 date=`date +%Y%m%d%H%M%S`
 
-if [[ -z ${env} || -z ${command} ]]; then
+# if [[ -z ${env} || -z ${command} ]]; then
+if [[ -z ${env} ]]; then
   echo 'one or more script variables are undefined'
   # echo "expecting: ./runactions.sh <environment name> <apply|destroy>"
-  echo "expecting: ./runactions.sh <environment name> <apply>"
+  echo "expecting: ./runactions.sh <environment name>"
   echo ""
   exit 1
 fi
 
 if [[ ${1} = *-* ]]; then
-  echo "environment name must not contain -. Use _ instead"
+  echo "environment name must not contain '-'. Use '_' instead"
   echo ""
   exit 1
 fi
@@ -27,19 +28,19 @@ if [[ ${#1} -lt 3 ]]; then
   exit 1
 fi
 
-case "${command}" in
-  apply|destroy)
-    ;;
-  *)
-    # echo "Accepted command for runactions.sh is one of: apply, destroy"
-    echo "Accepted command for runactions.sh is one of: apply"
-    echo ""
-    exit 1
-    ;;
-esac
+# case "${command}" in
+#   apply|destroy)
+#     ;;
+#   *)
+#     # echo "Accepted command for runactions.sh is one of: apply, destroy"
+#     echo "Accepted command for runactions.sh is one of: apply"
+#     echo ""
+#     exit 1
+#     ;;
+# esac
 
 /tf/rover/gorover.sh ${env} plan
-read -p 'Do you want to proceed with the apply via a pipeline (yes/no): ' proceed
+read -p 'Do you want to proceed with the deployment via a pipeline (yes/no): ' proceed
 
 if [ -z "$proceed" ];
 then
@@ -49,12 +50,14 @@ fi
 
 if [ "${proceed,,}" = "yes" ];
 then
+  read -p 'Please provide a commit message for this deployment: ' commit_message
+  
   mkdir -p .actions
   echo "/tf/rover/gorover.sh ${env} plan" > .actions/cicdrun.sh
-  echo "/tf/rover/gorover.sh $@ -auto-approve" >> .actions/cicdrun.sh
+  echo "/tf/rover/gorover.sh ${env} apply -auto-approve" >> .actions/cicdrun.sh
   chmod +x .actions/cicdrun.sh
   git add .
-  git commit -m "runactions ${env} ${command} in ${blueprint} on ${date}"
+  git commit -m "${commit_message}: runactions ${env} ${command} in ${blueprint} on ${date}"
   git tag "${blueprint}_${env}_${command}_${date}"
   git push
   git push --tags
